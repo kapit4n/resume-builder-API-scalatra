cf90317a66529b840d397f50caa59fafb4775346
larce <luis.arce22@gmail.com>
2018-01-25 08:13:35 -0400

    Adding more models to have the profile information

--- src/main/scala/com/resume/app/data.scala
+++ src/main/scala/com/resume/app/data.scala
@@ -1,9 +1,15 @@
 package com.resume.app
 
 object ResumeData {
+
+	var personalData = PersonalData("Luis Arce", "Software Architect")
+
 	var all = List(
-					Resume("Java developer", "Skills"),
-					Resume("Scala developer", "Skills"),
-					Resume("Python developer", "Skills")
+					Resume(PersonalData("Luis Arce", "Software Architect"), "Scala Developer", "Worked in some personal projects to enforce my skills",
+							List("Scala", "Play Framework", "Scalatra","Linq"), List("English", "Spanish"), 
+							List(Experience("2015", "2017", "Scala DEV", "Dyamsoft", List("Write Code"))), 
+							List("Write a list of code by day", "Read a book by month"),
+							List(Education("2005", "2010", "System Engineer", "UMSSS"))
+						)
 				)
 }
--- src/main/scala/com/resume/app/models.scala
+++ src/main/scala/com/resume/app/models.scala
@@ -1,3 +1,12 @@
 package com.resume.app
 
-case class Resume(title: String, skills: String)
+case class PersonalData(fullName: String, profession: String)
+
+case class Experience(fromDate: String, toDate: String, title: String, company: String, actions: List[String])
+
+case class Education(fromDate: String, toDate: String, subject: String, collage: String)
+
+case class Resume(personalData: PersonalData, title: String, summary: String, skills: List[String],
+					languages: List[String], experience: List[Experience], hobbies: List[String],
+					education: List[Education]
+				  )
